8246c458ec1a0b309d5ba73a1feb0284aafab75a
sneirabastos@miu.edu <sneirabastos@miu.edu>
2022-07-11 16:26:19 -0500

    Remove unnecessary annotation parameter.

--- eregister/src/main/java/edu/miu/eregister/controller/StudentRestController.java
+++ eregister/src/main/java/edu/miu/eregister/controller/StudentRestController.java
@@ -11,37 +11,37 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value ={"/eregister/api/v1/student"})
+@RequestMapping("/eregister/api/v1/student")
 public class StudentRestController {
 
     @Autowired
     private StudentService studentService;
 
-    @GetMapping(value = {"/list"})
+    @GetMapping("/list")
     public ResponseEntity<List<Student>> listStudents() {
-        return ResponseEntity.ok(studentService.getAllStudents());
+        return ResponseEntity.ok(studentService.getAll());
     }
 
-    @GetMapping(value = {"/get/{studentId}"})
+    @GetMapping("/get/{studentId}")
     public ResponseEntity<Student> getStudentId(@PathVariable Integer studentId) {
-        Optional<Student> student = studentService.getStudentById(studentId);
+        Optional<Student> student = studentService.get(studentId);
         return student.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping(value = {"/new"})
+    @PostMapping("/new")
     public ResponseEntity<Student> addNewStudent(@RequestBody StudentRequest studentRequest) {
-        return new ResponseEntity<>(studentService.addNewStudent(studentRequest),
+        return new ResponseEntity<>(studentService.add(studentRequest),
                 HttpStatus.CREATED);
     }
 
-    @PutMapping(value = {"/update/{studentId}"})
+    @PutMapping("/update/{studentId}")
     public ResponseEntity<Student> updateStudent(@RequestBody StudentRequest studentRequest) {
-        return ResponseEntity.ok(studentService.updateStudent(studentRequest));
+        return ResponseEntity.ok(studentService.update(studentRequest));
     }
 
-    @DeleteMapping(value = {"/delete/{studentId}"})
+    @DeleteMapping("/delete/{studentId}")
     public ResponseEntity<Void> deleteStudent(@PathVariable Integer studentId) {
-        boolean isDeleted = studentService.deleteStudentById(studentId);
+        boolean isDeleted = studentService.delete(studentId);
         return new ResponseEntity<>(isDeleted ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 }
